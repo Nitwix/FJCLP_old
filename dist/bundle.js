303261463e560461ff87d2cf17e1824a6f6a12c0
Niels Lachat <nielsnfsmw@gmail.com>
2018-07-11 17:03:44 +0200

    dernières tentatives

--- dist/bundle.js
+++
@@ -1,8 +0,0 @@
-define("index", ["require", "exports", "phaser"], function (require, exports, Phaser) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var game = new Phaser.Game({
-        width: 960,
-        height: 540,
-    });
-});
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 import 'phaser';
-
-const game = new Phaser.Game({
+var game = new Phaser.Game({
     width: 960,
     height: 540,
 });
